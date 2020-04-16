80794ed65cea369f5b4b41f27f2f50b9e152408c
fahim100 <fahimahmmed100@gmail.com>
2020-04-16 17:08:40 +0600

    Fixed responsiveness of home page.

--- src/js/script.js
+++ src/js/script.js
@@ -14,10 +14,33 @@ $(document).ready(function() {
 		arrows: true,
 		prevArrow: $('.prev'),
 		nextArrow: $('.next'),
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					arrows: false,
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+				}
+			}
+		]
 	});
 
 	// Match height plugin
 	$('.recent-slider-item').matchHeight({
 		byRow: 0
 	});
+
+	// counter up js 
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+	
 });
